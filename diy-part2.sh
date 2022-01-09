683adcd320bed2d17c05c37a35179941f25cb810
Soulmate99 <64270631+Soulmate99@users.noreply.github.com>
2022-01-09 20:05:05 +0800

    Update diy-part2.sh
    Add luci-app-chinadns-ng

--- diy-part2.sh
+++ diy-part2.sh
@@ -15,12 +15,8 @@ sed -i 's/192.168.1.1/192.168.10.1/g' package/base-files/files/bin/config_genera
 
 #添加额外软件包
 git clone https://github.com/rufengsuixing/luci-app-adguardhome.git package/luci-app-adguardhome
-#git clone https://github.com/maxlicheng/luci-app-ssr-plus.git package/luci-app-ssr-plus
-#git clone https://github.com/vernesong/OpenClash.git package/OpenClash
-#git clone https://github.com/destan19/OpenAppFilter.git package/OpenAppFilter
-#git clone https://github.com/zzsj0928/luci-app-pushbot.git package/luci-app-pushbot
-#git clone https://github.com/riverscn/openwrt-iptvhelper.git package/openwrt-iptvhelper
-#git clone https://github.com/jerrykuku/luci-app-jd-dailybonus.git package/luci-app-jd-dailybonus
+git clone -b luci https://github.com/pexcn/openwrt-chinadns-ng.git package/luci-app-chinadns-ng
+git clone https://github.com/pexcn/openwrt-chinadns-ng.git package/chinadns-ng
 
 #添加smartdns
 git clone https://github.com/pymumu/openwrt-smartdns package/smartdns

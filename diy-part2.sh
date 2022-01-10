cc602c417740737415c93f48a94ba0ac9f28be81
Soulmate99 <64270631+Soulmate99@users.noreply.github.com>
2022-01-10 14:02:00 +0800

    Update diy-part2.sh

--- diy-part2.sh
+++ diy-part2.sh
@@ -15,8 +15,8 @@ sed -i 's/192.168.1.1/192.168.10.1/g' package/base-files/files/bin/config_genera
 
 #添加额外软件包
 git clone https://github.com/rufengsuixing/luci-app-adguardhome.git package/luci-app-adguardhome
-git clone -b luci https://github.com/pexcn/openwrt-chinadns-ng.git package/luci-app-chinadns-ng
-git clone https://github.com/pexcn/openwrt-chinadns-ng.git package/chinadns-ng
+#git clone -b luci https://github.com/pexcn/openwrt-chinadns-ng.git package/luci-app-chinadns-ng
+#git clone https://github.com/pexcn/openwrt-chinadns-ng.git package/chinadns-ng
 
 #添加smartdns
 git clone https://github.com/pymumu/openwrt-smartdns package/smartdns

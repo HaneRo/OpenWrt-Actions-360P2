385fd60f4684e88bba8a29799227d051f6f0bac4
Soulmate99 <64270631+Soulmate99@users.noreply.github.com>
2022-01-11 14:39:41 +0800

    Update diy-part2.sh
    Add vnstat2

--- diy-part2.sh
+++ diy-part2.sh
@@ -15,6 +15,7 @@ sed -i 's/192.168.1.1/192.168.10.1/g' package/base-files/files/bin/config_genera
 
 #添加额外软件包
 git clone https://github.com/rufengsuixing/luci-app-adguardhome.git package/luci-app-adguardhome
+git clone https://github.com/eerrjjkk/luci-app-vnstat2.git package/luci-app-vnstat2
 #git clone -b luci https://github.com/pexcn/openwrt-chinadns-ng.git package/luci-app-chinadns-ng
 #git clone https://github.com/pexcn/openwrt-chinadns-ng.git package/chinadns-ng
 
